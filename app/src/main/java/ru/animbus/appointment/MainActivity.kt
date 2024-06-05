4cad5de793b37d9349012f57afd15ff2a3b71a0a
mmishkin <9mihail@gmail.com>
2024-06-05 18:12:32 +0300

    init commit

---
+++ app/src/main/java/ru/animbus/appointment/MainActivity.kt
@@ -0,0 +1,102 @@
+package ru.animbus.appointment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SetControlBtns()
+        }
+    }
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun SetControlBtns() {
+        Column(modifier = Modifier.fillMaxWidth()) {
+            Spacer(modifier = Modifier.height(height = 48.dp))
+            Row(modifier = Modifier.padding(all = 16.dp)){
+                ElevatedButton(
+                    onClick = {
+                        val intent = Intent(this@MainActivity, WebViewActivity::class.java)
+                        intent.putExtra("url", "https://vitamed-rm.ru/visit/specialty")
+                        startActivity(intent)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.teal_200))
+                ) {
+                    Text(
+                        text = "Запись Вита-Мед г.Саранск ",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                }
+            }
+            Row(modifier = Modifier.padding(all = 16.dp)){
+                ElevatedButton(
+                    onClick = {
+                        val intent = Intent(this@MainActivity, WebViewActivity::class.java)
+                        intent.putExtra("url", "https://kim-rm.ru/appointment-to-doctor")
+                        startActivity(intent)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.teal_200))
+                ) {
+                    Text(
+                        text = "Запись КИМ г.Саранск",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                }
+            }
+            Row(modifier = Modifier.padding(all = 16.dp)){
+                ElevatedButton(
+                    onClick = {
+                        val intent = Intent(this@MainActivity, WebViewActivity::class.java)
+                        intent.putExtra("url", "https://syst-assist.ru/site/index?area=95")
+                        startActivity(intent)
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = R.color.teal_200))
+                ) {
+                    Text(
+                        text = "Запись syst-assist ",
+                        style = MaterialTheme.typography.titleLarge,
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(height = 64.dp))
+            Image(
+                painter = painterResource(R.drawable.appointment_transparent),
+                contentDescription = null,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .size(300.dp)
+            )
+        }
+    }
+}
---
+++ app/src/main/java/ru/animbus/appointment/WebViewActivity.kt
@@ -0,0 +1,128 @@
+package ru.animbus.appointment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.OnBackPressedCallback
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+
+
+class WebViewActivity : ComponentActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            intent.getStringExtra("url")?.let { MainContent(url = it) }
+        }
+        onBackPressedDispatcher.addCallback(
+            this /* lifecycle owner */,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    val intent = Intent(this@WebViewActivity, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            })
+    }
+
+    // Creating a composable
+// function to display Top Bar
+    @Composable
+    fun MainContent(url: String) {
+        Scaffold(
+            content = { MyContent(url) }
+        )
+    }
+
+
+    // Creating a composable
+// function to create WebView
+// Calling this function as
+// content in the above function
+    @Composable
+    fun MyContent(url: String) {
+
+        // Declare a string that contains a url
+        val mUrl = url
+        Column {
+            // Adding a WebView inside AndroidView
+            // with layout as full screen
+            Row(modifier = Modifier.padding(all = 16.dp)) {
+                Image(
+                    painter = painterResource(R.drawable.ic_back),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .clickable { onBackPressedDispatcher.onBackPressed() }
+                        .size(40.dp)
+                        .clip(CircleShape)
+                        .border(1.5.dp, colorResource(id = R.color.teal_200), CircleShape)
+                )
+                Spacer(modifier = Modifier.width(width = 16.dp))
+                Text(
+                    text = getString(R.string.app_name),
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.titleLarge,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.align(alignment = Alignment.CenterVertically)
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .height(height = 1.dp)
+                    .fillMaxWidth()
+                    .background(Color(getColor(R.color.teal_200))),
+            )
+            AndroidView(factory = {
+                WebView(it).apply {
+                    settings.javaScriptEnabled = true
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = WebViewClient()
+                    loadUrl(mUrl)
+                }
+            }, update = {
+                it.loadUrl(mUrl)
+            })
+        }
+    }
+
+    // For displaying preview in
+// the Android Studio IDE emulator
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        MainContent("https://vitamed-rm.ru/visit/specialty")
+    }
+}
